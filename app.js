e95f7cb4d088616f9c5653d1091dba832a26d147
KrisssBrown <cristianbruno.cs@gmail.com>
2023-02-02 20:42:46 +0100

    START

---
+++ app.js
@@ -0,0 +1,41 @@
+console.log('ciao')
+
+
+for (let i=1; i<=100; i++){
+
+
+    const resto = i % 15
+
+
+    console.log(i, resto)
+
+
+
+     if (resto === 0){
+         console.log(i, 'fizzbuzz')
+     }
+
+     else if ((resto / 3) === 0){
+        console.log ('bu')
+     }
+}
+
+
+// for (let i=5; i<=100; i++){
+
+
+//     const resto = i % 5
+
+
+//     console.log(i, resto)
+
+
+
+//      if (resto === 0){
+//          console.log('buzz')
+//      }
+// }
+
+
+
+
