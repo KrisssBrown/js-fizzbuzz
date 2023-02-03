7a3db671efcd3446afc856736db553cd71c465bd
KrisssBrown <cristianbruno.cs@gmail.com>
2023-02-03 15:47:33 +0100

    END js

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 console.log('ciao')
 
 
-for (let i=1; i<=100; i++){
+for (let i=0; i<=100; i++){
+
+    let n = i + 1
+
+    const resto = i % 3
+    const resto5 = i % 5
+
+    let result = n
+
+    // console.log(i, resto)
 
 
-    const resto = i % 15
 
-
-    console.log(i, resto)
-
-
-
-     if (resto === 0){
+     if (resto === 0 && resto5 === 0){
          console.log(i, 'fizzbuzz')
      }
 
-     else if ((resto / 3) === 0){
-        console.log ('bu')
+     else if (resto === 0){
+        console.log ('fizz')
      }
+
+     else if (resto5 === 0){
+        console.log ('buzz')
+     }
+
+     console.log (result)
 }
 
 
